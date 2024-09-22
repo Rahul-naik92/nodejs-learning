d198504b9b5b8d00f0814eb208cc67448b9ef10b
rahulnaik92 <teamrahul.naik@gmail.com>
2024-09-22 09:52:08 +0530

    finding specific items

--- OS Module/Path Module/path_module.js
+++ OS Module/Path Module/path_module.js
@@ -5,4 +5,10 @@ console.log(path.dirname('D:/node_learning/OS Module/Path Module/path_module.js'
 
 console.log(path.extname('D:/node_learning/OS Module/Path Module/path_module.js'))
 
-console.log(path.dirname('D:/node_learning/OS Module/Path Module/path_module.js'))
+console.log(path.basename('D:/node_learning/OS Module/Path Module/path_module.js'))
+
+console.log(path.parse('D:/node_learning/OS Module/Path Module/path_module.js'))
+
+const myPath = path.parse('D:/node_learning/OS Module/Path Module/path_module.js')
+
+console.log(myPath.name)
