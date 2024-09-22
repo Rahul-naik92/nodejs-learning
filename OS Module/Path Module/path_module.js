66babae8147fc3abe23008d7b10d0c3e816b42cf
rahulnaik92 <teamrahul.naik@gmail.com>
2024-09-22 09:45:34 +0530

    usage path module

---
+++ OS Module/Path Module/path_module.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+
+console.log(path.dirname('D:/node_learning/OS Module/Path Module/path_module.js'))
+
+console.log(path.extname('D:/node_learning/OS Module/Path Module/path_module.js'))
+
+console.log(path.dirname('D:/node_learning/OS Module/Path Module/path_module.js'))
