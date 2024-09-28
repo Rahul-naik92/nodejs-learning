0afe134cb3aac70fa2ed4d990830d61970614fde
rahulnaik92 <teamrahul.naik@gmail.com>
2024-09-28 13:26:09 +0530

    event

--- eventMod/index.js
+++ eventMod/index.js
@@ -6,3 +6,13 @@
 //Ex: 1 - Registering for the event to the fired only one time using once.
 //Ex : 2 - Create an event emitter instance and register a coupld of callbacks
 //Ex - 3 - Registering for the event with callback parameters. 
+
+
+const EventEmitter=require('events')
+const event = new EventEmitter();
+
+event.on('SayMyName', ()=>{
+    console.log("My Name is Rahul");
+})
+
+event.emit('SayMyName');
