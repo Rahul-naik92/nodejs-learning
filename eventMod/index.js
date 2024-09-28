f6ad74c9f649fbb906afe4c8dab07c1151bc74bc
rahulnaik92 <teamrahul.naik@gmail.com>
2024-09-28 13:16:19 +0530

    event module

--- eventMod/index.js
+++ eventMod/index.js
@@ -1,1 +1,8 @@
-//Event Modile
+//Event Module
+
+//Node.js has a build-in module, called "Events"
+//Where you can create, fire, and listen for your own events.
+
+//Ex: 1 - Registering for the event to the fired only one time using once.
+//Ex : 2 - Create an event emitter instance and register a coupld of callbacks
+//Ex - 3 - Registering for the event with callback parameters. 
