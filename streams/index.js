5e0c36a0adcfb939e6cd7b36058a0c64bce8eaac
rahulnaik92 <teamrahul.naik@gmail.com>
2024-09-28 16:26:20 +0530

    streams and stream.pipe

---
+++ streams/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+const http = require("http")
+
+const server = http.createServer();
+
+server.on("request",(req, res)=>{
+
+    // var fs = require("fs");
+    // fs.readFile('streams.txt', (err, data)=>{
+    //     if(err) return console.log(err);
+    //     res.end(data.toString());
+    // });
+
+    const rstream = fs.createReadStream("streams.txt");
+    rstream.on('data',(chunkData)=>{
+console.log("readhing");
+        res.write(chunkData);
+
+    });
+
+    rstream.on('end', ()=>{
+        console.log("end");
+         res.end();
+    });
+
+    rstream.on('error', (err)=>{
+console.log(err);
+         res.end("File not found");
+    });
+});
+
+server.listen(8000, "127.0.0.1", ()=>{
+    console.log("Listening to the port no 8000");
+});
